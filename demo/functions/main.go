2bb3dd552c91f50d7138d88fc65226806baf8652
Ashish Juyal <ashish@thecodecamp.in>
2023-05-29 14:56:28 +0530

    functions

--- demo/functions/main.go
+++ demo/functions/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import "fmt"
 
+func swap(a, b int, name string) (int, int) {
+	return b, a
+}
+
+func Write(b []byte) (numberOfBytes int, err error) {
+	return 0, nil
+}
+
+func sum(a, b int) int {
+	return a + b
+}
+
+func print(msg string) {
+	fmt.Println(msg)
+}
+
+func main() {
+	defer releaseResources()
+	fmt.Println("testing defer statement")
+}
+
+func releaseResources() {
+	fmt.Println("closing and releasinng connections....")
 }
