7215d52cb1487f13362a40f1e37c2b1a87e63322
Ashish Juyal <ashish@thecodecamp.in>
2023-05-31 13:27:40 +0530

    running two mux server

--- demo/http/main.go
+++ demo/http/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	// server 1
+	mux := http.NewServeMux()
+	mux.HandleFunc("/greet", greet2)
+
+	go func() {
+		log.Println("starting server 1....")
+		log.Fatal(http.ListenAndServe("localhost:8081", mux))
+	}()
+
+	// server 2
+	http.HandleFunc("/greet", greet)
+	log.Println("starting server 2....")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!!"))
+}
+
+func greet2(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!! from new mux server"))
 }
