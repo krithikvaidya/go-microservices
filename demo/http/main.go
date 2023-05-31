8f47ab4578403b339c238ffbcfed542beb5ec45f
Ashish Juyal <ashish@thecodecamp.in>
2023-05-31 14:20:39 +0530

    added customer struct and using json.NewEncoder for customers

--- demo/http/main.go
+++ demo/http/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
+type Customer struct {
+	Id      string
+	Name    string
+	City    string
+	Zipcode string
+}
 
+func main() {
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getCustomers)
 
 	log.Println("starting server 1....")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
@@ -20,4 +28,14 @@ func greet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(201)
 	w.Write([]byte(msg))
+}
+
+func getCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"1", "Abhay", "Blr", "123456"},
+		{"2", "Ashish", "Del", "110011"},
+		{"3", "Rob", "Mum", "220011"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
