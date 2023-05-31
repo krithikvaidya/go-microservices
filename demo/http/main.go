906b2c9faaac5179240991aff8a4e49999e6fa22
Ashish Juyal <ashish@thecodecamp.in>
2023-05-31 13:48:22 +0530

    adding headers to the response

--- demo/http/main.go
+++ demo/http/main.go
@@ -1,30 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	// server 1
-	mux := http.NewServeMux()
-	mux.HandleFunc("/greet", greet2)
 
-	go func() {
-		log.Println("starting server 1....")
-		log.Fatal(http.ListenAndServe("localhost:8081", mux))
-	}()
-
-	// server 2
 	http.HandleFunc("/greet", greet)
-	log.Println("starting server 2....")
+
+	log.Println("starting server 1....")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!!"))
-}
+	msg := fmt.Sprintf("{ \"msg\": \"%s\"}", "Hello World!!")
 
-func greet2(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!! from new mux server"))
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(201)
+	w.Write([]byte(msg))
 }
