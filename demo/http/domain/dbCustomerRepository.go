9b51751aca4a82f92188c0bca7bca4f5d7cb1cf4
Ashish Juyal <ashish@thecodecamp.in>
2023-06-01 18:13:58 +0530

    handling sql.ErrNoRows

--- demo/http/domain/dbCustomerRepository.go
+++ demo/http/domain/dbCustomerRepository.go
@@ -2,6 +2,7 @@ package domain
 
 import (
 	"database/sql"
+	"errors"
 	"log"
 	"time"
 
@@ -13,8 +14,6 @@ type CustomerRepositoryDb struct {
 }
 
 func (cr *CustomerRepositoryDb) FindAll() ([]Customer, error) {
-	// db := getDbClient()
-
 	selectSQL := "SELECT customer_id, name, date_of_birth, city, zipcode, status from customers"
 	rows, err := cr.db.Query(selectSQL)
 	if err != nil {
@@ -36,28 +35,28 @@ func (cr *CustomerRepositoryDb) FindAll() ([]Customer, error) {
 }
 
 func (cr *CustomerRepositoryDb) ById(id string) (*Customer, error) {
-	// db := getDbClient()
-
 	selectSQL := "SELECT customer_id, name, date_of_birth, city, zipcode, status from customers where customer_id = ?"
-	rows, err := cr.db.Query(selectSQL, id)
+	row := cr.db.QueryRow(selectSQL, id)
 
+	if row.Err() != nil {
+		log.Println("Error while querying customer table: ", row.Err())
+		return nil, row.Err()
+	}
+
+	c := Customer{}
+	err := row.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
+
+	// https://stackoverflow.com/a/60123886
+	if err == sql.ErrNoRows {
+		log.Println("No rows found...", row.Err())
+		return nil, errors.New("Customer not found")
+	}
 	if err != nil {
-		log.Println("Error while querying customer table: ", err.Error())
+		log.Println("Error while scanning customer data: ", err.Error())
 		return nil, err
 	}
-	customers := make([]Customer, 0)
 
-	for rows.Next() {
-		c := Customer{}
-		err = rows.Scan(&c.Id, &c.Name, &c.DateOfBirth, &c.City, &c.Zipcode, &c.Status)
-		if err != nil {
-			log.Println("Error while scanning customer data: ", err.Error())
-			return nil, err
-		}
-		customers = append(customers, c)
-	}
-
-	return &customers[0], nil
+	return &c, nil
 }
 
 func getDbClient() *sql.DB {
