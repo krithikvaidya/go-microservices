57622555474c8bcfb7c6e8cf631391e29174732a
Ashish Juyal <ashish@thecodecamp.in>
2023-06-01 13:42:44 +0530

    reverting changes

--- demo/http/app/app.go
+++ demo/http/app/app.go
@@ -2,31 +2,21 @@ package app
 
 import (
 	"learning-http/handlers"
+	"log"
 	"net/http"
 
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 
-	// r := mux.NewRouter()
+	r := mux.NewRouter()
 
-	// r.HandleFunc("/greet", handlers.Greet)
-	// r.HandleFunc("/customers", handlers.GetCustomers).Methods(http.MethodGet)
-	// r.HandleFunc("/customers/{customer_id:[0-9]+}", handlers.GetCustomer).Methods(http.MethodGet)
-	// r.HandleFunc("/customers", handlers.NewCustomer).Methods(http.MethodPost)
+	r.HandleFunc("/greet", handlers.Greet)
+	r.HandleFunc("/customers", handlers.GetCustomers).Methods(http.MethodGet)
+	r.HandleFunc("/customers/{customer_id:[0-9]+}", handlers.GetCustomer).Methods(http.MethodGet)
+	r.HandleFunc("/customers", handlers.NewCustomer).Methods(http.MethodPost)
 
-	// log.Println("starting server ....")
-	// log.Fatal(http.ListenAndServe("localhost:8080", r))
-	r := gin.Default()
-
-	r.GET("/ping", ping)
-	r.GET("/customers", handlers.GetCustomers)
-	r.Run()
-}
-
-func ping(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+	log.Println("starting server ....")
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
