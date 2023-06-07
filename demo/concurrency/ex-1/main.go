9e87eceb80401dc29c31a7a9e5cb72b3ffe13dbb
Ashish Juyal <ashish@thecodecamp.in>
2023-06-07 16:17:34 +0530

    waitgroup as concurrent version

--- demo/concurrency/ex-1/main.go
+++ demo/concurrency/ex-1/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	printMessage("hello")
-	printMessage("world")
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go printMessage("hello", &wg)
+	go printMessage("world", &wg)
+	// go func() {
+	// 	printMessage("hello")
+	// 	wg.Done()
+	// }()
+
+	// go func() {
+	// 	printMessage("world")
+	// 	wg.Done()
+	// }()
+
+	wg.Wait() // blocking
 }
 
-func printMessage(msg string) {
-	for i := 1; true; i++ {
+func printMessage(msg string, wg *sync.WaitGroup) {
+	for i := 1; i <= 5; i++ {
 		fmt.Println(i, msg)
 		time.Sleep(time.Millisecond * 500)
 	}
+	wg.Done()
 }
