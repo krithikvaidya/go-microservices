87f18e30bb2eadec1c594abf8d9d92be62727a99
Ashish Juyal <ashish@thecodecamp.in>
2023-06-08 14:03:33 +0530

    step 1, splitting list in two

--- demo/concurrency/ex-3/main.go
+++ demo/concurrency/ex-3/main.go
@@ -14,17 +14,35 @@ func main() {
 func calculateSumOfNumbers(max int) {
 	s := GenerateNumbers(max)
 	t := time.Now()
+
+	part1 := s[:len(s)/2]
+	part2 := s[len(s)/2:]
+	ch := make(chan int64)
+
+	go sum(part1, ch)
+	go sum(part2, ch)
+
+	x := <-ch
+	y := <-ch
+	total := x + y
+
+	fmt.Printf("Channel Add, Sum: %d,  Time Taken: %s\n", total, time.Since(t))
+}
+
+func sum(part []int, ch chan int64) {
 	var sum int64
-	for _, v := range s {
+	for _, v := range part {
 		sum += int64(v)
 	}
+	ch <- sum
+}
 
-	fmt.Printf("Without channel Add, Sum: %d,  Time Taken: %s\n", sum, time.Since(t))
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
 // GenerateNumbers - random number generation
 func GenerateNumbers(max int) []int {
-	rand.Seed(time.Now().UnixNano())
 	numbers := make([]int, max)
 	for i := 0; i < max; i++ {
 		numbers[i] = rand.Intn(10)
