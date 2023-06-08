c6a1d4af9c4eaf2c2cd6ac0d378f69b086dd72ef
Ashish Juyal <ashish@thecodecamp.in>
2023-06-08 13:23:20 +0530

    channel example

--- demo/concurrency/ex-4/main.go
+++ demo/concurrency/ex-4/main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"time"
 )
 
 func main() {
+	ch := make(chan string)
+	go getMessage(ch)
 
+	for msg := range ch {
+		// msg, isOpen := <-ch
+		// if !isOpen {
+		// 	break
+		// }
+		fmt.Println(msg)
+	}
 }
 
 func getMessage(ch chan string) {
-	for i := 1; i < 5; i++ {
+	for i := 1; i <= 5; i++ {
 		ch <- time.Now().String()
 		time.Sleep(time.Millisecond * 500)
 	}
+	close(ch)
 }
